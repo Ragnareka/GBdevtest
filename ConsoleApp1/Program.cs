f43b6a97cd7052e45d9f92b7f17371e2fca54842
Anna-Mariya Z <mantikora97@yandex.ru>
2023-05-07 11:07:00 +0400

    program

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+       
+            private static void Main(string[] args)
+            {
+                {
+                    Console.Clear();
+                    Console.WriteLine(" имеющегося массива строк формирует новый массив из строк, длина которых меньше, либо равна 3 символам");
+                    Console.WriteLine("Введите массив. для разделения элементов массива используйте ;");
+                    string[] array = Console.ReadLine()!.Split(';');
+                PrintM(array);
+                   string[] result = new string[array.Length];
+                   int counts = 0;
+                    for ( int i=0;i<array.Length;i++)
+                    {
+                        if (array[i].Length > 2)
+                        {result[counts] = array[i];
+                        counts++;}
+                    }
+                    PrintM(result);
+                }
+
+            void PrintM(string[] matrix)
+            {
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {             
+                    if (!(string.IsNullOrEmpty(matrix[i])) )    
+                   { Console.Write($"{matrix[i]} \t");
+                    Console.WriteLine();}
+                }
+            }
+
+
+        }
+        }
+
+    
+}
